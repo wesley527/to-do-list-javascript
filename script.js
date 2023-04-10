6e75a17b5adfb119754751ed30a4d70394a3f2b4
Wesley Renato <wesley.renato.dj@gmail.com>
2023-04-10 14:23:36 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const localStoragekey = 'to-do-list-gn'
+
+function newTask()
+{
+    let input = document.getElementById('input-new-task')
+    // validação
+    if(!input.value)
+    {
+        alert('Digite algo pare inserir em sua lista')
+    }
+    // else if()
+    else{
+ // increment to localStorage
+ let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+ values.push({
+     name: input.value
+ })
+ localStorage.setItem(localStorageKey,JSON.stringify(values))
+ showValues()
+}
+input.value = ''
+}
+
+function showValues()
+{
+let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+let list = document.getElementById('to-do-list')
+list.innerHTML = ''
+for(let i = 0; i < values.length; i++)
+{
+ list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg></button></li>`
+}
+}
+
+function removeItem(data)
+{
+let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+let index = values.findIndex(x => x.name == data)
+values.splice(index,1)
+localStorage.setItem(localStorageKey,JSON.stringify(values))
+showValues()
+}
+
+showValues()
+
