87f000bb9acb8b982dc8a6867131e80b831f3663
Wesley Renato <wesley.renato.dj@gmail.com>
2023-04-10 21:23:08 -0300

    to-do-list-ltd

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # to-do-list-javascript
 To do list with HTML + CSS + JAVASCRIPT
+Agregando tarefas, aprendendo e praticando.
+
--- script.js
+++ script.js
@@ -1,45 +1,58 @@
-const localStoragekey = 'to-do-list-gn'
+const localStorageKey = 'to-do-list-ltd'
+
+function validateIfExistsNewTask() // Função para validar se já existe a task
+{
+    let values     = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let inputValue = document.getElementById('input-new-task').value
+    let exists     = values.find(x => x.name == inputValue)
+    return !exists ? false : true
+}
 
 function newTask()
 {
     let input = document.getElementById('input-new-task')
-    // validação
+    input.style.border = ''
+
+    // validation
     if(!input.value)
     {
-        alert('Digite algo pare inserir em sua lista')
+        input.style.border = '1px solid red'
+        alert('Digite algo para inserir em sua lista')
     }
-    // else if()
-    else{
- // increment to localStorage
- let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
- values.push({
-     name: input.value
- })
- localStorage.setItem(localStorageKey,JSON.stringify(values))
- showValues()
+    else if(validateIfExistsNewTask())
+    {
+        alert('Já existe uma task com essa descrição')
+    }
+    else
+    {
+        // increment to localStorage // JSON para transformar String em Array
+        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+        values.push({
+            name: input.value
+        })
+        localStorage.setItem(localStorageKey,JSON.stringify(values))
+        showValues()
+    }
+    input.value = ''
 }
-input.value = ''
+function showValues() // Função responsavel por mostrar os valores do localStorage
+{
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let list = document.getElementById('to-do-list')
+    list.innerHTML = ''
+    for(let i = 0; i < values.length; i++) // Laço de repetição
+    {
+        list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg></button></li>`
+    }
 }
 
-function showValues()
+function removeItem(data) // Função para remover task
 {
-let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
-let list = document.getElementById('to-do-list')
-list.innerHTML = ''
-for(let i = 0; i < values.length; i++)
-{
- list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg></button></li>`
-}
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let index = values.findIndex(x => x.name == data)
+    values.splice(index,1)
+    localStorage.setItem(localStorageKey,JSON.stringify(values))
+    showValues()
 }
 
-function removeItem(data)
-{
-let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
-let index = values.findIndex(x => x.name == data)
-values.splice(index,1)
-localStorage.setItem(localStorageKey,JSON.stringify(values))
 showValues()
-}
-
-showValues()
-
